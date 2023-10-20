9380351ad6fe2b5caf230c51e071f3a4a66ff49f
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-10-20 23:38:46 +0300

    burger v01

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*swiper start*/
 var swiper = new Swiper(".mySwiper", {
     navigation: {
       nextEl: ".swiper-button-next",
@@ -8,3 +9,21 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
       },
   });
+/*swiper end*/
+
+
+
+/*burger start*/
+document.addEventListener(".DOMContentLoaded", function() {
+  let burger = document.querySelector(".header__burger");
+  let headerMenu = document.querySelector(".header__menu")
+
+  burger.addEventListener("click", function() {
+    if (headerMenu.computedStyleMap.left === "-100%") {
+      headerMenu.computedStyleMap.left = "0";
+    } else {
+      headerMenu.style.left = "100%";
+    }
+  });
+});
+/*burger end*/
