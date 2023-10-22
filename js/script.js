f429f38085ff0cfa2e8bc8929e6618fb6ddc9ecf
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-10-22 18:59:52 +0300

    Adapted header and 4 sections

--- js/script.js
+++ js/script.js
@@ -27,3 +27,17 @@ document.addEventListener(".DOMContentLoaded", function() {
   });
 });
 /*burger end*/
+
+
+
+/*header backgeound start*/
+let header = document.querySelector(".header");
+
+window.onscroll = function() {
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    header.style.backgroundColor = "rgb(75%, 75%, 75%, 0.7)";
+  } else {
+    header.style.backgroundColor = "rgb(5%, 5%, 5%, 0)";
+  }
+}
+/*header backgeound end*/
