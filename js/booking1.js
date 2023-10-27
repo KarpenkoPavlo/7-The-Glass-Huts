ad29b2e0f5dde859612cb995eb6ff9ab534270d9
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-10-27 12:45:07 +0300

    Finished multi-level menu and custom checkboxes

--- js/booking1.js
+++ js/booking1.js
@@ -9,3 +9,33 @@ window.onscroll = function() {
   }
 }
 /*header backgeound end*/
+
+
+
+/*hidden start*/
+document.addEventListener("DOMContentLoaded", function () {
+  let buttonsHidden = document.querySelectorAll(".choose__button");
+  let articlesHidden = document.querySelectorAll(".chooseItem__hidden");
+
+  buttonsHidden.forEach((button, index) => {
+    let arrowDown = button.querySelector(".arrowDown");
+    let arrowUp = button.querySelector(".arrowUp");
+
+    button.addEventListener("click", function () {
+      if (articlesHidden[index].style.display === "block") {
+        articlesHidden[index].style.display = "none";
+        arrowDown.style.display = "block";
+        arrowUp.style.display = "none";
+      } else {
+        articlesHidden[index].style.display = "block";
+        arrowDown.style.display = "none";
+        arrowUp.style.display = "block";
+      }
+    });
+  });
+});
+
+
+
+
+/*hidden end*/
