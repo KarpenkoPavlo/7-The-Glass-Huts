e19db2cd9964d9d590237b769cbd50cda0643930
Pavlo Karpenko <karpenkopasha19@outlook.com>
2024-01-14 22:13:53 +0200

    Block form in payment.html.CheckBox in addInfo

--- js/payment.js
+++ js/payment.js
@@ -9,3 +9,21 @@ window.onscroll = function() {
   }
 }
 /*header backgeound end*/
+
+/*------------------------------------------------------------------------------------------*/
+
+/* CheckBox */
+function checkBox() {
+  let checkBox = document.querySelector('.checkbox');
+  let currentData = checkBox.getAttribute('data');
+
+  if(currentData === 'normal') {
+    checkBox.src = "/img/choose/checkboxActive.svg";
+    checkBox.setAttribute('data', 'active');
+  } else if(currentData === 'active') {
+    checkBox.src = "/img/choose/checkboxNormal.svg";
+    checkBox.setAttribute('data', 'normal');
+  }
+}
+
+document.querySelector('.checkbox').addEventListener('click', checkBox);
