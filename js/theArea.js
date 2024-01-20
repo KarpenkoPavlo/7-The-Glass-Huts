3022e1040c56b77e9be2df52fe3ef08c6f24ac52
Pavlo Karpenko <karpenkopasha19@outlook.com>
2024-01-20 14:48:02 +0200

    Completed theArea.html. Start AboutUs.html

---
+++ js/theArea.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      // pagination: {
+      //   el: ".swiper-pagination",
+      //   type: "fraction",
+      // },
+  });
