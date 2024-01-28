e500fa8134b188eb64c449cd1ca01bf0262bb3b3
Pavlo Karpenko <karpenkopasha19@outlook.com>
2024-01-28 21:14:03 +0200

    ContactUs correct

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -1,3 +1,6 @@
+// Якщо натискаємо на 'contact us' то в коді нижче:
+// overlay - це блок на всю сторінку сірого кольору який стане видимим
+// contactUs__container - це блок з формами який стає видимим
 function contactUs() {
     let contactUs__container = document.querySelector('.contactUs__container');
     let overlay = document.querySelector('.overlay');
@@ -7,10 +10,12 @@ function contactUs() {
         contactUs__container.style.display = 'block';
         overlay.style.display = 'block';
         document.body.classList.add('no-scroll');
-        wrapper.style.marginRight = '8px';
+        // wrapper.style.marginRight = '8px';
     } 
 }
-
+// Якщо натискаємо на 'SEND' то в коді нижче:
+// overlay - стане не видимим
+// contactUs__container - стане не видимим
 function aboutButtHide() {
     let contactUs__container = document.querySelector('.contactUs__container');
     let overlay = document.querySelector('.overlay');
@@ -20,6 +25,12 @@ function aboutButtHide() {
         contactUs__container.style.display = 'none';
         overlay.style.display = 'none';
         document.body.classList.remove('no-scroll');
-        wrapper.style.marginRight = '-8px';
+        // wrapper.style.marginRight = '-8px';
     }
 }
+// Це можливість закрити блок з формами кнопкой Escape = Esc
+document.addEventListener('keydown', (event) => {
+    if(event.code == 'Escape') {
+        aboutButtHide();
+    }
+})
